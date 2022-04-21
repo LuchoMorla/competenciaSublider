088628ec42b62dfba5a5ba6b2e303f67b9f74130
morlucho <luis-fernando-10@outlook.es>
2022-04-20 23:21:19 -0500

    update final

--- participantes.js
+++ participantes.js
@@ -2,32 +2,54 @@ var idImagenes = [];
 idImagenes["samuel"] = "https://scontent.fuio1-1.fna.fbcdn.net/v/t1.6435-9/161432833_102932598551242_7090641754533412937_n.jpg?_nc_cat=104&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=lPlQK02uAgQAX_L7zOS&_nc_ht=scontent.fuio1-1.fna&oh=00_AT8bz-FS_cpoXqGkR_CqikIE8ffiGsj9xOxYlJ3QWAGyKA&oe=6287078A";
 idImagenes["josue"] = "./imagen2.jpeg";
 idImagenes["Emilio"] = "./imagen1.jpeg";
+idImagenes["Sanchez"] = "https://s1.zerochan.net/Donquixote.Doflamingo.600.2313934.jpg";
+idImagenes["Cesar"] = "./WhatsApp Image 2022-04-19 at 10.02.37 PM.jpeg";
 
 var puntajeSamuel = 0;
 var puntajeJosue = 0;
 var puntajeEmilio = 0;
+var puntajeSanchez = 0;
+var puntajeCesar = 0;
 
 var puntajes = [];
 puntajes["samuel"] = puntajeSamuel;
 puntajes["josue"] = puntajeJosue;
 puntajes["Emilio"] = puntajeEmilio;
+puntajes["Sanchez"] = puntajeSanchez;
+puntajes["Cesar"] = puntajeCesar;
 
+var partidasGanadas = [];
+partidasGanadas["samuel"] = 0;
+partidasGanadas["josue"] = 0;
+partidasGanadas["Emilio"] = 0;
+partidasGanadas["Sanchez"] = 0;
+partidasGanadas["Cesar"] = 0;
+
+// funcion que imprima por consola el puntaje total del personaje con su nombre
+function imprimirPuntaje(nombre, puntajeGanador) {
+    console.log(nombre + ": " + puntajeGanador);
+}
 
 class participante {
-    constructor(id, n, cN, p) {
+    constructor(id, n, cN) {
         this.imagen = new Image();
         this.id = id;
         this.name = n;
         this.characterName = cN;
         this.puntos = puntajes[this.name];
+        this.victorias = partidasGanadas[this.name];
         this.imagen.src = idImagenes[this.name];
+        this.puntajeTotal = this.puntos + (this.victorias * 5000);
     }
     mostrar(){
       document.body.appendChild(this.imagen);
-      document.write("<p>");
+      document.write("<br>");
       document.write("<strong>" + this.id + "</strong>");
       document.write(") Nombre: " + this.characterName + " <br>");
-      document.write("Puntaje en competencias: " + this.puntos + " ");
+      document.write("Score en competencias: " + this.puntos + "<br> ");
+      document.write("Numero de victorias: " + this.victorias + "<br> ");
+      imprimirPuntaje(this.name, this.puntajeTotal);
+      document.write("Puntaje total: " + this.puntajeTotal + "<br> ");
       document.write("</p>");
       document.write("<hr />");
     }
@@ -37,13 +59,16 @@ class participante {
 var participante1 = new participante(01, "samuel", "AOKAMYA",);
 var participante2 = new participante(02,"josue", "Empanada2");
 var participante3 = new participante(03, "Emilio", "Emilio_OP");
+var participante4 = new participante(04, "Sanchez", "Joaovitor");
+var participante5 = new participante(05, "Cesar", "TzKaizen");
 
 var participantes = [];
 participantes.push(participante1);
 participantes.push(participante2);
 participantes.push(participante3);
+participantes.push(participante4);
+participantes.push(participante5);
 
 for (data of participantes) {
     data.mostrar();
 }
-
